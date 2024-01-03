24944fa5466d06e54622c039ab72f03d3df6661d
Parth <parthjohi009@gmail.com>
2024-01-03 13:27:52 +0530

    add

--- controller/demo.js
+++ controller/demo.js
@@ -7,6 +7,9 @@ const demoSchema = new mongoose.Schema({
   lastName: {
     type: mongoose.Schema.Types.String,
   },
+  email: {
+    type: mongoose.Schema.Types.String,
+  },
 });
 
 module.exports = new mongoose.model("Demo", demoSchema);
--- controller/demoData.js
+++ controller/demoData.js
@@ -7,6 +7,7 @@ const createDemoData = async function (req,res){
     const demoData = await demoSchema.create({
         firstName:"parth",
         lastName:"Joshi",
+        email:"parthjohi099@gmail.com"
     })
     // const {firstName,lastName} = req.body
     res.send({Message:"user created successfuly",demoData})
