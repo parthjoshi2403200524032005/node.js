dca0759bff3256f2648c5edfc16142d23b0fdec1
Parth <parthjohi009@gmail.com>
2024-01-03 13:16:24 +0530

    add

--- controller/demoData.js
+++ controller/demoData.js
@@ -2,11 +2,13 @@
 
 const demoSchema = require('./demo.js')
 
+
 const createDemoData = async function (req,res){
     const demoData = await demoSchema.create({
         firstName:"parth",
         lastName:"Joshi",
     })
+    const {firstName,lastName} = req.body
     res.send({Message:"user created successfuly",demoData})
 }
 
--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const route = require("./route/routes");
 const { default: mongoose } = require("mongoose");
 const app = express();
 
-
-
-
-mongoose.connect("mongodb+srv://parthjoshi7639:ParthJoshi@cluster0.pxjkjcm.mongodb.net/", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.error("MongoDB connection error:", err));
-
 app.use("/",route) 
+
+
+
+  mongoose.connect("mongodb+srv://parthjohi009:E5ESleWwGGp0waa1@cluster0.k3a6ktn.mongodb.net/?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+   
+  });
+
+
+
+
+
+
 
 app.listen(7000,()=>{
     console.log("port is connected")
